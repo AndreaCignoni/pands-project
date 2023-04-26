63c402a9d9c90cc8671ee28a0796f9c4cc016967
unknown <andreacignoni@hotmail.com>
2023-04-26 16:48:29 +0100

    Adding Scatterplots

--- analysis.py
+++ analysis.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 # Reading and formatting the data set downloaded (https://archive.ics.uci.edu/ml/machine-learning-databases/iris/)
 
 df = pd.read_csv("iris.csv", header=None,)
-df.columns= ['sepal_lenght','sepal_width','petal_lenght','petal_width','class']
+df.columns= ['sepal_length','sepal_width','petal_length','petal_width','class']
 
 # this shows first and last five raws of all data  
 """""
@@ -33,7 +33,7 @@ print (df)
 # select setosa and versicolor and virginica
 y = df.iloc[0:150, 4].values
 y = np.where(y == 'Iris-setosa', 0, 1)
-
+"""""
 # extract sepal length and petal length
 X = df.iloc[0:150, [0, 2]].values
 
@@ -51,11 +51,11 @@ plt.legend(loc='upper left')
 
 # plt.show()
 
-# extract sepal width and petal width
+# extract sepal and petal width
 
 X = df.iloc[0:150, [1, 3]].values
 
-# scatterplot width data
+# scatterplot sepal and petal width data
 plt.scatter(X[:50, 0], X[:50, 1],
             color='blue', marker= 'o', label='Setosa')
 plt.scatter(X[50:100, 0], X[50:100, 1],
@@ -69,3 +69,38 @@ plt.legend(loc='upper right')
 
 # plt.show()
 
+# extract petal width and length
+
+X = df.iloc[0:150, [2, 3]].values
+
+# scatterplot petal width and length data
+plt.scatter(X[:50, 0], X[:50, 1],
+            color='blue', marker= 'o', label='Setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1],
+            color='green', marker='s', label='Versicolor')
+plt.scatter(X[100:150, 0], X[100:150, 1],
+            color='red', marker='*', label='Versicolor')
+
+plt.xlabel('Petal length [cm]')
+plt.ylabel('Petal width [cm]')
+plt.legend(loc='upper left')
+
+plt.show()
+"""""
+# extract sepal width and length
+
+X = df.iloc[0:150, [0, 1]].values
+
+# scatterplot sepal width and length data
+plt.scatter(X[:50, 0], X[:50, 1],
+            color='blue', marker= 'o', label='Setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1],
+            color='green', marker='s', label='Versicolor')
+plt.scatter(X[100:150, 0], X[100:150, 1],
+            color='red', marker='*', label='Versicolor')
+
+plt.xlabel('Sepal length [cm]')
+plt.ylabel('Sepal width [cm]')
+plt.legend(loc='upper right')
+
+plt.show()
